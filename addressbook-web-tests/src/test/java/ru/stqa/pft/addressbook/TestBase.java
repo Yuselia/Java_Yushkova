b9dc8756181d08f9bf8c22efa0d741be0754bc19
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-01-05 23:33:57 +0500

    Из теста по созданию контакта выделены методы в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -80,4 +80,35 @@ public class TestBase {
   protected void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
   }
+
+  protected void gotoHomePage() {
+      wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void submitContactCreation() {
+      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).click();
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+      wd.findElement(By.name("lastname")).click();
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+      wd.findElement(By.name("address")).click();
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      wd.findElement(By.name("theform")).click();
+      wd.findElement(By.name("home")).click();
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+      wd.findElement(By.name("mobile")).click();
+      wd.findElement(By.name("mobile")).clear();
+      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
+  }
+
+  protected void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
+  }
 }
