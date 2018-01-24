a6ab6f94e9e14fb55baf1f6a066015e93106ff2b
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-01-24 14:42:40 +0500

    Добавлены проверки изменил количества контактов в тестах создания и удаления контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -12,9 +13,12 @@ public class ContactDeletionTests extends TestBase {
         if (!app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("name", "Sourname", "test1","Test street", "12345", "89271111111", "yuselia@yandex.ru"));
         }
+        int before=app.getContactHelper().getContactCount();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContacts();
         app.getNavigationHelper().gotoHomePage();
+        int after=app.getContactHelper().getContactCount();
+        Assert.assertEquals(after, before-1);
     }
 
 }
