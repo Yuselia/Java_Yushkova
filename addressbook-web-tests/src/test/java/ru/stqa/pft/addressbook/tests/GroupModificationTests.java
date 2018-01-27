53ec181c11b383b6a3eb260a40c64c8a17f055d8
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-01-28 00:16:36 +0500

    Сравнение списков групп до и после выполнения тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -22,6 +22,10 @@ public class GroupModificationTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after=app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    after.remove(after.size()-1);
+    before.remove(before.size()-1);
+    Assert.assertEquals(before, after);
   }
 
 }
