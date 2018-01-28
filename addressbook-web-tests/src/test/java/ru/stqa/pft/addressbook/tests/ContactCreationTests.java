13273dab6cd8b428153efa97e24e419086506ef8
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-01-28 21:16:26 +0500

    Поиск элемента с максимальным id в тесте создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,8 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,7 +23,8 @@ public class ContactCreationTests extends TestBase {
                 max=с.getId();
             }
         }
-        contact.setId(max);
+
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
