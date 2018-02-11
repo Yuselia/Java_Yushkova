55ef0c2ac77af11a27df01051d1b75fcad7c80a3
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-02-12 02:06:52 +0500

    Добавлена проверка почтового адреса и адресов электронной почты на главной странице приложения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -108,22 +108,32 @@ public class ContactHelper extends HelperBase {
       int id=Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
       String lastName = cells.get(1).getText();
       String firstName = cells.get(2).getText();
-      String allphones = cells.get(5).getText();
-      contactCache.add(new ContactData().withId(id).withName(firstName).withLastname(lastName).
-              withAllPhones(allphones));
+      String address=cells.get(3).getText();
+      String allEmails = cells.get(4).getText();
+      String allPhones = cells.get(5).getText();
+      contactCache.add(new ContactData().withId(id).withName(firstName).withLastname(lastName).withAddress(address)
+              .withAllPhones(allPhones)
+              .withAllEmails(allEmails));
     }
     return new Contacts(contactCache);
   }
 
   public ContactData infoFromEditForm(ContactData contact) {
     initContactModificationById(contact.getId());
+
     String firstname=wd.findElement(By.name("firstname")).getAttribute("value");
     String lastname=wd.findElement(By.name("lastname")).getAttribute("value");
+    String address=wd.findElement(By.name("address")).getText();
     String home=wd.findElement(By.name("home")).getAttribute("value");
     String mobile=wd.findElement(By.name("mobile")).getAttribute("value");
     String work=wd.findElement(By.name("work")).getAttribute("value");
+    String email=wd.findElement(By.name("email")).getAttribute("value");
+    String email2=wd.findElement(By.name("email2")).getAttribute("value");
+    String email3=wd.findElement(By.name("email3")).getAttribute("value");
+
     wd.navigate().back();
-    return new ContactData().withId(contact.getId()).withName(firstname).withLastname(lastname)
-            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+    return new ContactData().withId(contact.getId()).withName(firstname).withLastname(lastname).withAddress(address)
+            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work)
+            .withEmail(email).withEmail2(email2).withEmail3(email3);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInformationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInformationTests.java
@@ -9,21 +9,30 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactInformationTests extends TestBase {
 
   @Test
-  public void testContactPhones() {
+  public void testContactInformation() {
     app.goTo().homePage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s)->!s.equals(""))
+            .map(ContactInformationTests::cleaned)
+            .collect(Collectors.joining("\n"));
   }
 
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
             .stream().filter((s)->!s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactInformationTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
