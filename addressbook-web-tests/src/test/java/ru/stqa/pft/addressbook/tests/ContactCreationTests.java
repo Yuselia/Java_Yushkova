435b7d740d04b742c71385201621599b98358342
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-02-18 23:28:36 +0500

    Параметризация тестового метода создания контактов (часть 1)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {"test 1", "lastname 1", "address 1", "test1", "12345", "89271111111", "yuselia@yandex.ru", "src/test/resources/stru.png"});
+        list.add(new Object[] {"test 2", "lastname 2", "address 2", "test1", "12345", "89271111111", "yuselia@yandex.ru", "src/test/resources/stru.png"});
+        list.add(new Object[] {"test 3", "lastname 3", "address 3", "test1", "12345", "89271111111", "yuselia@yandex.ru", "src/test/resources/stru.png"});
+        return list.iterator();
+    }
+
+    @Test (dataProvider = "validContacts")
+    public void testContactCreation(String name, String lastname, String address, String group, String homePhone, String mobilePhone, String email, String pathToPhoto) {
+        File photo = new File(pathToPhoto);
+        ContactData contact = new ContactData().withName(name).withLastname(lastname).withGroup(group)
+               .withAddress(address).withHomePhone(homePhone).withMobilePhone(mobilePhone)
+               .withEmail(email).withPhoto(photo);
         app.goTo().homePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/stru.png");
-        ContactData contact = new ContactData().withName("name").withLastname("Sourname").withGroup("test1")
-                .withAddress("Test street").withHomePhone("12345").withMobilePhone("89271111111")
-                .withEmail("yuselia@yandex.ru").withPhoto(photo);
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size()+1));
         Contacts after = app.contact().all();
