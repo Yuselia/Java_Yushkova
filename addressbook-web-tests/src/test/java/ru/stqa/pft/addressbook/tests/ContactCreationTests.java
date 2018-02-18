5c6c4432a5538c1329d465f11f7f3c71d098ae28
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-02-19 00:08:54 +0500

    Загрузка тестовых данных контактов из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,18 +16,26 @@ import static org.hamcrest.MatcherAssert.*;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         File photo = new File("src/test/resources/stru.png");
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new ContactData().withName("test 1").withLastname("lastname 1").withAddress("address 1")
-                .withGroup("test1").withHomePhone("12345").withMobilePhone("89271111111")
-        .withEmail("yuselia@yandex.ru").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withName("test 2").withLastname("lastname 1").withAddress("address 1")
-                .withGroup("test1").withHomePhone("12345").withMobilePhone("89271111111")
-                .withEmail("yuselia@yandex.ru").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withName("test 3").withLastname("lastname 1").withAddress("address 1")
-                .withGroup("test1").withHomePhone("12345").withMobilePhone("89271111111")
-                .withEmail("yuselia@yandex.ru").withPhoto(photo)});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line!=null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withName(split[0])
+            .withLastname(split[1])
+            .withAddress(split[2])
+            .withHomePhone(split[3])
+            .withMobilePhone(split[4])
+            .withWorkPhone(split[5])
+            .withEmail(split[6])
+            .withEmail2(split[7])
+            .withEmail3(split[8])
+            .withGroup(split[9])
+            .withPhoto(photo)});
+            line=reader.readLine();
+        }
         return list.iterator();
     }
 
