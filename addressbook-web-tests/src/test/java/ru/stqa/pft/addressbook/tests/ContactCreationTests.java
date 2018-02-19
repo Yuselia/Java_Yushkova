799ae77a52433db5f6bb70956b442b859e6503f5
Yulia Yushkova <yuselia@MacBook-Air-Yushkova.local>
2018-02-20 00:23:40 +0500

    Добавлено автоматическое закрытие ресурсов с помощью блока try

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -20,46 +20,50 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class ContactCreationTests extends TestBase {
 
-    @DataProvider
-    public Iterator<Object[]> validContactsFromXml() throws IOException {
-        File photo = new File("src/test/resources/stru.png");
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-        String xml = "";
-        String line = reader.readLine();
-        while (line != null) {
-            xml += line;
-            line = reader.readLine();
-        }
-        XStream xstream = new XStream();
-        xstream.processAnnotations(ContactData.class);
-        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-        return contacts.stream().map((c) -> new Object[] {c.withPhoto(photo)}).collect(Collectors.toList()).iterator();
-    }
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    File photo = new File("src/test/resources/stru.png");
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((c) -> new Object[]{c.withPhoto(photo)}).collect(Collectors.toList()).iterator();
 
-    @DataProvider
-    public Iterator<Object[]> validContactsFromJson() throws IOException {
-        File photo = new File("src/test/resources/stru.png");
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null) {
-            json += line;
-            line = reader.readLine();
-        }
-        Gson gson = new Gson();
-        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); //List<ContactData>.class
-        return contacts.stream().map((c) -> new Object[]{c.withPhoto(photo)}).collect(Collectors.toList()).iterator();
     }
+  }
 
-    @Test (dataProvider = "validContactsFromJson")
-    public void testContactCreation(ContactData contact) {
-
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        app.contact().create(contact);
-        assertThat(app.contact().count(), equalTo(before.size()+1));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(
-                before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    File photo = new File("src/test/resources/stru.png");
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+      }.getType()); //List<ContactData>.class
+      return contacts.stream().map((c) -> new Object[]{c.withPhoto(photo)}).collect(Collectors.toList()).iterator();
     }
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
+
+    app.goTo().homePage();
+    Contacts before = app.contact().all();
+    app.contact().create(contact);
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
 }
